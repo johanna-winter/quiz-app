8b96acaeb268a5e15758023b04c1a333a3aea8d1
Johanna Winter <johanna.winter@gmx.de>
2025-10-08 15:30:51 +0200

    add button functionality

---
+++ js/index.js
@@ -0,0 +1,16 @@
+const answerButton = document.querySelector('[data-js="answer-button"]');
+const answerText = document.querySelector('[data-js="answer"]');
+const bookmarkIcon = document.querySelector('[data-js="bookmark"]');
+
+bookmarkIcon.addEventListener("click", () => {
+  bookmarkIcon.classList.toggle("question-card__bookmark--filled");
+});
+
+answerButton.addEventListener("click", () => {
+  answerText.classList.toggle("question-card__answer--hidden");
+  answerText.classList.toggle("question-card__answer--active");
+  const changeText = answerText.classList.contains(
+    "question-card__answer--active"
+  );
+  answerButton.textContent = changeText ? "Hide answer" : "Show answer";
+});
